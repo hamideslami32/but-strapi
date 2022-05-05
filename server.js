9f994433e57568c487ea7aea556bfe9b8111b799
Hamid Eslami <hesalmi32@gmail.com>
2022-05-05 16:45:08 +0000

    update: server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const strapi = require('strapi');
+
+// start strapi application
+strapi().start();
+
+
+/but-strapi
+└─── api
+└─── build
+└─── config
+└─── extensions
+└─── public
+- favicon.ico
+- package.json
+- server.js
