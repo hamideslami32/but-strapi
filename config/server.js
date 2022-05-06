ec4214a88907b1ce46a0476566f514f2db090635
hamid <heslami32@gmail.com>
2022-05-06 18:29:26 +0430

    change: server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
+  url: 'https://valimohebbi.com/strapi',
   app: {
     keys: env.array('APP_KEYS'),
   },
